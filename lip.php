d010285ad1a3e1e8056ea7962ed5a1c62fe0eee9
flapflapp <143656991+flapflapp@users.noreply.github.com>
2024-03-05 18:53:25 +0100

    Update lip.php ig

--- lip.php
+++ lip.php
@@ -1,6 +1,9 @@
 <?php
-$ipAddress = $_GET['ip'] ?? '';
-$file = fopen('lip.txt', 'a');
-fwrite($file, $ipAddress . "\n");
-fclose($file);
+$ip_address = $_SERVER['REMOTE_ADDR'];
+$date_time = date('Y-m-d H:i:s');
+$log_entry = "$date_time - $ip_address" . PHP_EOL;
+$log_file = 'lip.txt';
+file_put_contents($log_file, $log_entry, FILE_APPEND);
+header('Location: thank-you.html');
+exit;
 ?>
