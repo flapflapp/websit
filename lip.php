08f5715dda2d315cf1c864a2336eb5f5f3084ed3
flapflapp <143656991+flapflapp@users.noreply.github.com>
2024-03-05 19:01:14 +0100

    Delete lip.php

--- lip.php
+++
@@ -1,9 +0,0 @@
-<?php
-$ip_address = $_SERVER['REMOTE_ADDR'];
-$date_time = date('Y-m-d H:i:s');
-$log_entry = "$date_time - $ip_address" . PHP_EOL;
-$log_file = 'lip.txt';
-file_put_contents($log_file, $log_entry, FILE_APPEND);
-header('Location: thank-you.html');
-exit;
-?>
